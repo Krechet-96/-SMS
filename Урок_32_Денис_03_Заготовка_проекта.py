03d7bf26bc02061dd5c32179b60c6f40d89e7292
Programmer <shakirov.ekb@gmail.com>
2024-12-04 00:00:44 +0500

    Добавлена обработка исключений

--- Урок_32_Денис_03_Заготовка_проекта.py
+++ Урок_32_Денис_03_Заготовка_проекта.py
@@ -8,11 +8,13 @@ text='Hello world!'
 
 url=f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
 print(url)
-response = requests.get(url)
-print(response)
+try:
+    response = requests.get(url)
+    print(response)
 
-if response.status_code == 200:
-    print("Сообщение успешно отправлено!")
-else:
-    print(f"Ошибка при отправке сообщения {response.status_code}")
-
+    if response.status_code == 200:
+        print("Сообщение успешно отправлено!")
+    else:
+        print(f"Ошибка при отправке сообщения {response.status_code}")
+except Exception as e:
+    print(f"Непредвиденная ошибка с кодом {e}")
