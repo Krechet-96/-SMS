59728f95cc4f461563c941f8c6a0314feb2362ac
Programmer <shakirov.ekb@gmail.com>
2024-12-04 00:18:08 +0500

    Добавлена проверка корректности номера с помощью регулярных выражений.

---
+++ Урок_32_Денис_05_Проверка_корректности_номера.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+
+def validate_phone_number(phone_number):
+    pattern = r'^\d{11}$'
+    return bool(re.match(pattern, phone_number))
+
+
+user='user_name' # Своё имя будет
+password='svoypassword'
+sender='podpisotpravitelya'
+receiver='79022604437'
+text='Hello world!'
+
+if not validate_phone_number(receiver):
+    print("Ошибка! Некорректный номер ")
+else:
+    url=f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
+    print(url)
+    try:
+        response = requests.get(url)
+        print(response)
+
+        if response.status_code == 200:
+            print("Сообщение успешно отправлено!")
+        else:
+            print(f"Ошибка при отправке сообщения {response.status_code}")
+    except Exception as e:
+        print(f"Непредвиденная ошибка с кодом {e}")
